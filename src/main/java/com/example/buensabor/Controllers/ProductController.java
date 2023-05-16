cbc95034b3bd48097e31eb0d06ba2a3afbf05fee
SebastianSulia <angel.sulia@alumnos.frm.utn.edu.ar>
2023-05-16 15:11:32 -0300

    Busqueda de productos por categoría y por nombre

--- src/main/java/com/example/buensabor/Controllers/ProductController.java
+++ src/main/java/com/example/buensabor/Controllers/ProductController.java
@@ -3,9 +3,9 @@ package com.example.buensabor.Controllers;
 
 import com.example.buensabor.Models.Entity.Product;
 import com.example.buensabor.Services.Impl.ProductServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -14,6 +14,26 @@ public class ProductController extends BaseControllerImpl<Product, ProductServic
 
     public ProductController(ProductServiceImpl service) {
         super(service);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<?> getByCategory(@PathVariable String category){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.getByCategory(category));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente luego\"}");
+        }
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<?> getByName(@PathVariable String name){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.getByName(name));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente luego\"}");
+        }
     }
 
 
