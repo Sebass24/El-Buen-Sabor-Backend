9c4956f4eb506539622000c28ebeffb63358e840
SebastianSulia <angel.sulia@alumnos.frm.utn.edu.ar>
2023-05-17 15:18:05 -0300

    Agrego cors permitidos

--- src/main/java/com/example/buensabor/Services/ProductService.java
+++ src/main/java/com/example/buensabor/Services/ProductService.java
@@ -9,7 +9,15 @@ import java.util.Map;
 
 public interface ProductService extends BaseService<Product,Long> {
     //Agregamos todos los metodos que solo pertenecen a User, lo agregamos a la interfaz para mantener el orden
+
+    /**
+     * Filtra los productos por categoria.
+     *
+     * @param category string con el nombre de la categoria a filtrar
+     * @return Devuelve una lista de productos filtrado por categoria.
+     */
     public List<Product> getByCategory(String category);
+
     public List<Product> getByName(String name);
     public Map<String,Integer> getProductRanking(String category);
     public List<Product> getBestSellersFromToByCategory(int top, ProductCategory category, Date from, Date to);
