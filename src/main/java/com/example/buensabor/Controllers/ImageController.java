d91fc177e8e2626911ba1f7a4c88083aec0a77f9
SebastianSulia <angel.sulia@alumnos.frm.utn.edu.ar>
2023-05-29 20:12:44 -0300

    Agrego controlador para subir imagenes

--- src/main/java/com/example/buensabor/Controllers/ImageController.java
+++ src/main/java/com/example/buensabor/Controllers/ImageController.java
@@ -2,9 +2,10 @@ package com.example.buensabor.Controllers;
 
 import com.example.buensabor.Models.Entity.Image;
 import com.example.buensabor.Services.Impl.ImageServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin("*")
@@ -13,6 +14,19 @@ public class ImageController extends BaseControllerImpl<Image, ImageServiceImpl>
 
     public ImageController(ImageServiceImpl service) {
         super(service);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file){
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.save(file));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Al cargar imagen\"}");
+        }
+
+
     }
 
 
--- src/main/java/com/example/buensabor/Models/Entity/Image.java
+++ src/main/java/com/example/buensabor/Models/Entity/Image.java
@@ -1,10 +1,13 @@
 package com.example.buensabor.Models.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Table(name="image")
@@ -14,4 +17,7 @@ import lombok.NoArgsConstructor;
 public class Image extends Base{
     private String name;
     private String path;
+    @Transient
+    @JsonIgnore
+    private MultipartFile imagen;
 }
