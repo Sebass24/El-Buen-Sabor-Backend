898caf4cd985cd28ab6b7592777075a0feb10220
SebastianSulia <angel.sulia@alumnos.frm.utn.edu.ar>
2023-05-29 20:12:44 -0300

    Agrego controlador para subir imagenes

--- src/main/java/com/example/buensabor/Repositories/ImageRepository.java
+++ src/main/java/com/example/buensabor/Repositories/ImageRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends BaseRepository<Image,Long> {
+    
 }
--- src/main/java/com/example/buensabor/Services/Impl/ImageServiceImpl.java
+++ src/main/java/com/example/buensabor/Services/Impl/ImageServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.buensabor.Services.Impl;
+import com.example.buensabor.Exceptions.ServiceException;
 import com.example.buensabor.Models.Entity.Image;
 import com.example.buensabor.Repositories.ImageRepository;
 import com.example.buensabor.Services.ImageService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Service
 public class ImageServiceImpl extends BaseServiceImpl<Image,Long> implements ImageService {
@@ -14,4 +20,36 @@ public class ImageServiceImpl extends BaseServiceImpl<Image,Long> implements Ima
         this.imageRepository = imageRepository;
     }
 
+    @Transactional
+    public Image save(MultipartFile img) throws ServiceException {
+        try {
+            if (img.isEmpty()) {
+                throw new ServiceException("Empty image file");
+            }
+
+            // Guardar el archivo en el sistema de archivos
+            String fileName = UUID.randomUUID().toString() + "-" + img.getOriginalFilename();
+            String filePath = new File("").getAbsolutePath() + "/src/main/resources/Imges/" + fileName;
+            File dest = new File(filePath);
+            img.transferTo(dest);
+
+            Image entity = new Image(fileName,filePath,img);
+            entity = baseRepository.save(entity);
+            return entity;
+        }catch (Exception e) {
+            throw new ServiceException(e.getMessage());
+        }
+    }
+
+//    @Transactional
+//    public Image update(MultipartFile img) throws ServiceException {
+//        try {
+//            if (entity.getId() == null) {
+//                throw new ServiceException("La entidad a modificar debe contener un Id.");
+//            }
+//            return baseRepository.save(entity);
+//        }catch (Exception e) {
+//            throw new ServiceException(e.getMessage());
+//        }
+//    }
 }
