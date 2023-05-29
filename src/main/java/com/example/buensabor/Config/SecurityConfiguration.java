776cfea72ac5dba8af4a1e74d2aa28410707b455
francoGonzalez2706 <franco.gonzalez.reale.276@gmail.com>
2023-05-29 18:31:04 -0300

    changes auth0

--- src/main/java/com/example/buensabor/Config/SecurityConfiguration.java
+++ src/main/java/com/example/buensabor/Config/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration {
                 .requestMatchers("/api/v1/admin").hasAuthority("Admin")
                 .requestMatchers("/**").permitAll()
                 .and().cors().configurationSource(corsConfigurationSource())
-                .and().oauth2ResourceServer().jwt();
+                .and().csrf().disable().oauth2ResourceServer().jwt();
         http.csrf().disable();
         return http.build();
     }
@@ -51,8 +51,9 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList(corsAllowedOrigins));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
         configuration.setMaxAge(3600L);
--- src/main/java/com/example/buensabor/Controllers/OrderController.java
+++ src/main/java/com/example/buensabor/Controllers/OrderController.java
@@ -3,9 +3,9 @@ package com.example.buensabor.Controllers;
 
 import com.example.buensabor.Models.Entity.Order;
 import com.example.buensabor.Services.Impl.OrderServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -18,5 +18,14 @@ public class OrderController extends BaseControllerImpl<Order, OrderServiceImpl>
         super(service);
     }
 
+    @GetMapping("byStatus/{status}")
+    public ResponseEntity<?> getById(@PathVariable String status){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.getByStatus(status));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente luego\"}");
+        }
+    }
 
 }
